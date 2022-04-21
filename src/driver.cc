2a98aa51ebf3145ffee45cc3639be4eab48d0a29
DonLarry <donlarry.timoteo@gmail.com>
2022-04-20 23:33:55 -0400

    driver.cpp: moved, renamed and modified a little

--- src/driver.cc
+++ src/driver.cc
@@ -1,13 +1,17 @@
 #include "driver.hh"
 #include "parser.tab.hh"
+#include <sstream>
+#include <fstream>
+#include <iostream>
 
-driver::driver()
-  : trace_parsing(false), trace_scanning(false)
+driver::driver(/*std::ostream &out*/)
+  : out(* new std::stringstream(std::ios_base::out)), trace_parsing(false), trace_scanning(false)
 {}
 
 int driver::parse(const std::string& f)
 {
   file = f;
+  outputFile = f.empty() ? "out.cpp" : f.substr(0, f.find_last_of('.')) + ".cpp";
   location.initialize(&file);
   scan_begin();
   yy::parser parser(*this);
