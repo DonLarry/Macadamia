0320ff40d699fb38cb010488cf533c914ff2a415
DonLarry <donlarry.timoteo@gmail.com>
2022-04-20 23:32:58 -0400

    driver.hh: moved and modified a little

--- inc/driver.hh
+++ inc/driver.hh
@@ -1,6 +1,7 @@
 #ifndef DRIVER_HH
 #define DRIVER_HH
 
+#include <iostream>
 #include <string>
 #include "parser.tab.hh"
 
@@ -8,22 +9,25 @@
   yy::parser::symbol_type yylex (driver& drv)
 YY_DECL;
 
-class driver
+struct driver
 {
-public:
-  driver();
   // std::map<std::string, int> variables;
-  int parse(const std::string& file);
   int result;
   std::string file;
+  std::stringstream& out;
+  std::string outputFile;
   // Whether to generate parser debug traces.
   bool trace_parsing;
-  void scan_begin();
-  void scan_end();
   // Whether to generate scanner debug traces.
   bool trace_scanning;
   // The token's location used by the scanner.
   yy::location location;
+
+  int parse(const std::string& file);
+  void scan_begin();
+  void scan_end();
+
+  driver();
 };
 
 #endif // ! DRIVER_HH
